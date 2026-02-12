d8988b27e18b73a55fc8ff395ad2ca04bf318a7d
Sophia Marielle C. Mendoza <sophiamarielle.mendoza-24@cpu.edu.ph>
2026-02-13 06:13:34 +0800

    chore: install jest and Supertest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: "node",
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  testMatch: ["**/__tests__/**/*.test.js"],
+  testTimeout: 15000,
+  forceExit: true,
+};
